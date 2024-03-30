4de93d12ef39d4766b206d663c2505f085aedc3e
Conrado1212 <conrado1212@gmail.com>
2024-03-30 18:42:23 +0100

    sticky navbar ready

--- tributePage/app/app.js
+++ tributePage/app/app.js
@@ -1,5 +1,19 @@
-$("button").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".second").offset().top},
-        'slow');
+$(document).ready(function() {
+    var NavY = $('.nav').offset().top;
+
+    var stickyNav = function(){
+        var ScrollY = $(window).scrollTop();
+
+        if (ScrollY > NavY) {
+            $('.nav-menu').addClass('sticky');
+        } else {
+            $('.nav-menu').removeClass('sticky');
+        }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function() {
+        stickyNav();
+    });
 });
